739f4201a1587fdf0db2da34a7b02e06e5fcebb9
Josh Girvin <josh.girvin@gmail.com>
2014-03-04 22:50:36 +1000

    Setting up test routes

--- bootstrap.php
+++ bootstrap.php
@@ -2,14 +2,4 @@
 /**
  * Bootstrap file for bringing in autloading, etc
  */
-use Slim\Slim;
-
 require("vendor/autoload.php");
-/**
-$app = new Slim();
-
-$app->get("/", function() {
-    print("<h1>Hello, World!</h1>");
-});
-
-$app->run();**/
--- public/index.php
+++ public/index.php
@@ -1,5 +1,35 @@
 <?php
 /**
- * The front controller
+ * The front controller for Authe
+ * For testing and as a basic implementation
  */
+use Slim\Slim; 
+
 require("../bootstrap.php");
+
+$app = new Slim();
+
+$app->get("/", function() {
+    print("<h1>Hello, World!</h1>");
+});
+
+$app->get("/login", function() {
+    print("Login page goes here");
+});
+
+$app->get("/api/user/", function() {
+
+});
+
+$app->post("/api/login/", function() use ($app) {
+
+});
+
+/**
+ * A temporary hack until I write a better API
+ */
+$app->get("/api/logout/", function() {
+
+});
+
+$app->run();
